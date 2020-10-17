60ed7b1b9cd97abd75593592948e87d800d07e9d
unknown <aayousry@gmail.com>
2020-10-17 13:38:52 -0400

    Basic structure for testing

---
+++ hassan.abdelrahman/src/test/java/hassan/abdelrahman/FairShareApplicationTests.java
@@ -0,0 +1,13 @@
+package hassan.abdelrahman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
